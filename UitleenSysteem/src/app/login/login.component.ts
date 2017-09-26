7019d10be96168cb4e26e7a709805600f986ab52
Emile Bigot <emile.bigot@student.hu.nl>
2017-09-26 21:34:04 +0200

    Added login css

--- UitleenSysteem/src/app/login/login.component.ts
+++ UitleenSysteem/src/app/login/login.component.ts
@@ -5,22 +5,23 @@ import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'login',
-  template: `
-  <div> {{ (user | async)?.uid }} </div>
-  <button (click)="login('admin@admin.nl', 'test123')">Login</button>
-  <button (click)="logout()">Logout</button>
-  `,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
   user: Observable<firebase.User>;
+  loginError: boolean;
 
   constructor(public afAuth: AngularFireAuth) {
     this.user = afAuth.authState;
+    this.loginError = false;
   }
 
+  // admin@admin.nl test123 werkt altijd voor login
   login(email: string, password: string) {
     this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    this.afAuth.authState.subscribe((auth) => auth == null ? this.loginError = true : this.loginError = false);
   }
 
   logout() {
